3fd37db5f8cb1cd0ca3f29455c9415a2f675e91f
Alina Shestopalova <117362222+AlinaS13@users.noreply.github.com>
2023-03-16 18:16:30 +0200

    Update Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -35,3 +35,45 @@ Modal.propTypes = {
   largeImg: PropTypes.string.isRequired,
   onModalClose: PropTypes.func.isRequired,
 };
+
+
+// mport React, { useRef, useEffect } from 'react';
+// import s from './Modal.module.css';
+// import PropTypes from 'prop-types';
+
+// export const Modal = ({ modaiImg, toggleModal }) => {
+//   const overlay = useRef();
+
+//   useEffect(() => {
+//     overlay.current.focus();
+//   }, []);
+
+//   return (
+//     <div
+//       ref={overlay}
+//       tabIndex={-1}
+//       onKeyDown={event => {
+//         if (event.code !== 'Escape') {
+//           return;
+//         }
+//         toggleModal();
+//       }}
+//       className={s.backdrop}
+//       onClick={e => {
+//         if (e.target.nodeName !== 'DIV') {
+//           return;
+//         }
+//         toggleModal(e);
+//       }}
+//     >
+//       <div className={s.Modal}>
+//         <img src={modaiImg} alt="" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// Modal.propTypes = {
+//   modaiImg: PropTypes.string,
+//   toggleModal: PropTypes.func,
+// };
